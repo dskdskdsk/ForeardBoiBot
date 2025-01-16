87c8b1848cdd474122f31a587298062308a5ff52
dskdskdsk <106891382+dskdskdsk@users.noreply.github.com>
2025-01-16 08:54:01 -0600

    Update forwardboibot.py

--- forwardboibot.py
+++ forwardboibot.py
@@ -169,5 +169,5 @@ async def main():
         print("Бот запущений.")
         await check_channels()  # Запускаємо автоматичну перевірку
 
-if name == "__main__":
+if __name__ == "__main__":
     asyncio.run(main())
